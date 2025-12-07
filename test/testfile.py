4d8f15af383e92ff36b7ab089ac3608a1e52a1bb
codest <generalcatmouse@gmail.com>
2025-12-07 15:11:14 +0100

    Updated on Sun Dec  7 15:11:14 WAT 2025

---
+++ test/testfile.py
@@ -0,0 +1,14 @@
+# tests/testfile.py
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_trigger_error():
+    response = client.get("/trigger-error")
+    assert response.status_code == 500
+
+def test_stats_endpoint():
+    response = client.get("/stats")
+    assert response.status_code == 200
+
